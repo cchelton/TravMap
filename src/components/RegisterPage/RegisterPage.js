7de4edbd4e735790d0cc67f98ce46aa9f64273b5
cchelton <59346941+cchelton@users.noreply.github.com>
2020-05-04 15:03:56 -0500

    initial commit
    registration boilerplate works for my user table as of this commit.
    My user table cols:
    username, password, first_name, last_name, date_of_birth, moderator

---
+++ src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
+
+class RegisterPage extends Component {
+  state = {
+    username: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+    date_of_birth: "",
+  };
+
+  registerUser = (event) => {
+    event.preventDefault();
+
+    if (this.state.username && this.state.password) {
+      this.props.dispatch({
+        type: "REGISTER",
+        payload: {
+          username: this.state.username,
+          password: this.state.password,
+          first_name: this.state.first_name,
+          last_name: this.state.last_name,
+          date_of_birth: this.state.date_of_birth,
+        },
+      });
+    } else {
+      this.props.dispatch({ type: "REGISTRATION_INPUT_ERROR" });
+    }
+  }; // end registerUser
+
+  handleInputChangeFor = (propertyName) => (event) => {
+    this.setState({
+      [propertyName]: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.errors.registrationMessage && (
+          <h2 className="alert" role="alert">
+            {this.props.errors.registrationMessage}
+          </h2>
+        )}
+        <form className="formPanel" onSubmit={this.registerUser}>
+          <h1>Register User</h1>
+          <div>
+            <label htmlFor="username">
+              Username:
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleInputChangeFor("username")}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="password">
+              Password:
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleInputChangeFor("password")}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="first_name">
+              First Name:
+              <input
+                type="text"
+                name="first_name"
+                value={this.state.first_name}
+                onChange={this.handleInputChangeFor("first_name")}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="last_name">
+              Last Name:
+              <input
+                type="text"
+                name="last_name"
+                value={this.state.last_name}
+                onChange={this.handleInputChangeFor("last_name")}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="date_of_birth">
+              Date of Birth:
+              <input
+                type="text"
+                name="date_of_birth"
+                value={this.state.date_of_birth}
+                onChange={this.handleInputChangeFor("date_of_birth")}
+              />
+            </label>
+          </div>
+
+          <div>
+            <input
+              className="register"
+              type="submit"
+              name="submit"
+              value="Register"
+            />
+          </div>
+        </form>
+        <center>
+          <button
+            type="button"
+            className="link-button"
+            onClick={() => {
+              this.props.dispatch({ type: "SET_TO_LOGIN_MODE" });
+            }}
+          >
+            Login
+          </button>
+        </center>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(RegisterPage);
