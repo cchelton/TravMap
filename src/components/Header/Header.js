015ce23d8273840d8041502f7230e981f8ea2068
cchelton <59346941+cchelton@users.noreply.github.com>
2020-05-05 11:58:35 -0500

    created header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MapIcon from "@material-ui/icons/Map";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import SecurityIcon from "@material-ui/icons/Security";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function Header(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" component="h6">
+            TravMap
+          </Typography>
+          <IconButton>
+            <SecurityIcon />
+          </IconButton>
+          <IconButton>
+            <MapIcon />
+          </IconButton>
+          <IconButton>
+            <AccountCircleIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
