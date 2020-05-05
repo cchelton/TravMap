8875fa8f7502e31166307bbb7299c518154bfb13
cchelton <59346941+cchelton@users.noreply.github.com>
2020-05-05 16:02:59 -0500

    map now inherits size from contianer

--- src/views/UserHomePage/UserHomePage.js
+++ src/views/UserHomePage/UserHomePage.js
@@ -1,9 +1,19 @@
 import React from "react";
 import TheMap from "../../components/TheMap/TheMap";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  theMapContainer: {
+    display: "inline-block",
+    height: 500,
+    width: 700,
+  },
+}));
 
 function UserHomePage() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.theMapContainer}>
       <TheMap />
     </div>
   );
