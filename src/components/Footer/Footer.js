0201e9d0eaed00e2918087d6a4d31d4e4d72718c
cchelton <59346941+cchelton@users.noreply.github.com>
2020-05-05 14:05:08 -0500

    removed unneeded imports

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-  AppBar,
-  Typography,
-  Toolbar,
-  Divider,
-  ButtonBase,
-} from "@material-ui/core";
+import { AppBar, Typography, ButtonBase } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
