b2d0f6db908ff59496f33f7c0100ea59281b6972
cchelton <59346941+cchelton@users.noreply.github.com>
2020-05-05 14:01:46 -0500

    created footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Divider,
+  ButtonBase,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    top: "auto",
+    bottom: 0,
+  },
+  toolbar: {
+    display: "flex",
+    flexGrow: 1,
+    justifyContent: "flex-end",
+  },
+  friendsList: {
+    padding: "5px 24px",
+    display: "inline-block",
+    borderLeft: "solid rgba(0, 0, 0, 0.54) 1px",
+  },
+}));
+
+function Footer(props) {
+  const classes = useStyles();
+  return (
+    <AppBar className={classes.appBar}>
+      <div className={classes.toolbar}>
+        <ButtonBase className={classes.friendsList}>
+          <Typography component="h6">Friends</Typography>
+        </ButtonBase>
+      </div>
+    </AppBar>
+  );
+}
+
+export default Footer;
