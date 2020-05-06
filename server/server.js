6ce461613bb4264cb214d08647fe0605897d98c7
cchelton <59346941+cchelton@users.noreply.github.com>
2020-05-06 16:29:41 -0500

    Drop Zone uploads image w/ preview(unformatted)

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require("./strategies/user.strategy");
 const userRouter = require("./routes/user.router");
 const imageRouter = require("./routes/image.router");
 const moderationRouter = require("./routes/moderation.router");
+const uploaderS3Router = require("react-s3-uploader/s3router");
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -27,6 +28,15 @@ app.use(passport.session());
 app.use("/api/user", userRouter);
 app.use("/api/image", imageRouter);
 app.use("/api/moderation", moderationRouter);
+app.use(
+  "/s3",
+  uploaderS3Router({
+    bucket: "travmap-bucket", // required
+    region: "us-east-2", // optional
+    headers: { "Access-Control-Allow-Origin": "*" }, // optional
+    ACL: "public-read", // this is the default - set to `public-read` to let anyone view uploads
+  })
+);
 
 // Serve static files
 app.use(express.static("build"));
--- src/components/DropZone/DropZone.js
+++ src/components/DropZone/DropZone.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DropZoneS3Uploader from "react-dropzone-s3-uploader";
 import { withStyles, Menu, MenuItem } from "@material-ui/core";
 
 const StyledMenu = withStyles({
@@ -33,6 +34,23 @@ function DropZone(props) {
   const anchorEl = props.anchorEl;
   const handleClose = props.handleClose;
 
+  //
+  // S3Uploader Stuff
+
+  const handleFinishedUpload = (info) => {
+    console.log("File uploaded with filename", info.filename);
+    console.log("Access it on s3 at", info.fileUrl);
+  };
+
+  const uploadOptions = {
+    server: "http://localhost:5000",
+    signingUrlQueryParams: { uploadType: "avatar" },
+  };
+  const s3Url = "https://travmap-bucket.s3.amazonaws.com";
+
+  //  end S3Uploader Stuff
+  //
+
   return (
     <StyledMenu
       id="customized-menu"
@@ -42,7 +60,12 @@ function DropZone(props) {
       onClose={handleClose}
     >
       <MenuItem>
-        <h2>DROP ZONE</h2>
+        <DropZoneS3Uploader
+          onFinish={handleFinishedUpload}
+          s3Url={s3Url}
+          maxSize={1024 * 1024 * 5}
+          upload={uploadOptions}
+        />
       </MenuItem>
     </StyledMenu>
   );
