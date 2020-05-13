82948c7df927ce41e40c4ba62ad9d5b498d60758
cchelton <59346941+cchelton@users.noreply.github.com>
2020-05-13 13:52:32 -0500

    adds /add POST for adding friends

--- server/routes/friend.router.js
+++ server/routes/friend.router.js
@@ -5,6 +5,8 @@ const {
   rejectUnauthenticated,
 } = require("../modules/authentication-middleware");
 
+// TODO: learn pg-promise!
+
 // gets a user's friends list
 router.get("/:userID", rejectUnauthenticated, (req, res) => {
   const userID = req.params.userID;
@@ -32,6 +34,84 @@ router.get("/checkStatus", rejectUnauthenticated, (req, res) => {
 });
 
 // make friend request
+router.post("/add", (req, res) => {
+  const userID = req.query.userID;
+  const friendID = req.query.friendID;
+
+  const queryData = [userID, friendID];
+  let queryText = `SELECT "id" FROM "user_relationship"
+  WHERE "user_id" = $2 AND "friend_id" = $1;`;
+
+  // first, check for an outstanding friend request from the friend to the user
+  pool
+    .query(queryText, queryData)
+    .then((response) => {
+      if (response.rows.length > 0) {
+        queryText = `DELETE FROM "user_relationship"
+        WHERE ("user_id" = $1 AND "friend_id" = $2) OR ("user_id" = $2 AND "friend_id" = $1);`;
+        // if there are outstanding requests, delete them and...
+        pool
+          .query(queryText, queryData)
+          .then((response) => {
+            queryText = `INSERT INTO "user_relationship" ("user_id", "friend_id", "confirmed_request", "display_user_photos_on_friend_map")
+        VALUES ($1, $2, TRUE, TRUE), ($2, $1, TRUE, TRUE);`;
+
+            // create a confirmed relationship between both users because both users have now added each other.
+            pool
+              .query(queryText, queryData)
+              .then((response) => {
+                res.sendStatus(200); // DONE! send OK
+              })
+              .catch((err) => {
+                console.log(
+                  `Friend Request Error in: if there are outstanding requests, delete them and...:`
+                );
+                console.log(err);
+                console.log(`queryText:`, queryText);
+
+                res.sendStatus(500);
+              });
+          })
+          .catch((err) => {
+            // err logs
+            console.log(
+              `Friend Request Error in: if there are outstanding requests, delete them and...:`
+            );
+            console.log(err);
+            console.log(`queryText:`, queryText);
+
+            res.sendStatus(500);
+          });
+      } else {
+        queryText = `INSERT INTO "user_relationship" ("user_id", "friend_id", "confirmed_request", "display_user_photos_on_friend_map")
+        VALUES ($1, $2, FALSE, TRUE);`;
+
+        // if there are no outstanding requests, create one from the user to the friend.
+        pool
+          .query(queryText, queryData)
+          .then((response) => {
+            res.sendStatus(200); //  DONE! send OK
+          })
+          .catch((err) => {
+            console.log(
+              `Friend Request Error in: check for outstanding request:`
+            );
+            console.log(err);
+            console.log(`queryText:`, queryText);
+
+            res.sendStatus(500);
+          });
+      }
+    })
+    .catch((err) => {
+      // err logs
+      console.log(`Friend Request Error in: check for outstanding request:`);
+      console.log(err);
+      console.log(`queryText:`, queryText);
+
+      res.sendStatus(500);
+    });
+});
 
 // confirms incoming friend request
 router.put("/confirm/:incomingID");
